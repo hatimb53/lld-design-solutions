c38d43ca42234729dbab1b0c75ba2354b4d48ddb
hatimb53 <hatim.bohra523@gmail.com>
2022-04-17 20:29:18 +0530

    replaced with interface

--- src/distributed_task_executor/Application.java
+++ src/distributed_task_executor/Application.java
@@ -2,6 +2,7 @@ package distributed_task_executor;
 
 import distributed_task_executor.handlers.DistributedTaskManager;
 import distributed_task_executor.interfaces.ClientInterface;
+import distributed_task_executor.interfaces.TaskExecutionInterface;
 import distributed_task_executor.nodes.Client;
 import distributed_task_executor.tasks.examples.AddTask;
 import distributed_task_executor.tasks.examples.PrintTask;
@@ -11,16 +12,16 @@ public class Application {
         int MAX_THREAD_POOL=10;
         int MAX_WORKERS=20;
 
-        DistributedTaskManager distributedTaskManager=new DistributedTaskManager(MAX_THREAD_POOL,MAX_WORKERS);
+        TaskExecutionInterface taskExecutionInterface=new DistributedTaskManager(MAX_THREAD_POOL,MAX_WORKERS);
 
 
         int count=1;
         for(int i=0;i<5;i++) {
-            ClientInterface client = new Client("client-"+count++,distributedTaskManager);
+            ClientInterface client = new Client("client-"+count++,taskExecutionInterface);
             client.submitTask(new AddTask());
         }
         for(int i=0;i<6;i++) {
-            ClientInterface client = new Client("client-"+count++,distributedTaskManager);
+            ClientInterface client = new Client("client-"+count++,taskExecutionInterface);
             client.submitTask(new PrintTask());
         }
 
